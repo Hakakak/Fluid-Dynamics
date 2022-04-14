d6c8726646b1db93947be13402491484fe513bcf
Hakakak <hayk.harutyunyan.y@tumo.org>
2022-04-14 18:41:44 +0400

    added Input System

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const data = require("./data.json");
 
+// app.use(express.static("."));
 app.use(express.static(__dirname + "/"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "./index.html")
-})
+app.set("port", process.env.PORT || 3000);
 
-app.set("port", process.env.PORT || 8080)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
 
 app.listen(app.get("port"), () => {
     console.log("Server started on " + app.get("port"));
-})
+});
+
+io.sockets.emit("data", {data: data.data})
